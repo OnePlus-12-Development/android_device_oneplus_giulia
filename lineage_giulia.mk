4f8180d745cef7252fad1b89b4a0408870e9f603
madmax7896 <madmax78968@gmail.com>
2025-12-22 13:37:52 +0000

    giulia: Initial device tree conversion
    Change-Id: I598e93d42815c222975b1372a6135e79f5917e4f

---
+++ lineage_giulia.mk
@@ -0,0 +1,31 @@
+#
+# Copyright (C) 2025 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from giulia device
+$(call inherit-product, device/oneplus/giulia/device.mk)
+
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+PRODUCT_NAME := lineage_giulia
+PRODUCT_DEVICE := giulia
+PRODUCT_MANUFACTURER := OnePlus
+PRODUCT_BRAND := OnePlus
+PRODUCT_MODEL := CPH2691
+
+PRODUCT_GMS_CLIENTID_BASE := android-oneplus
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BuildDesc="qssi_64-user 16 BP2A.250605.015 1765278503964 release-keys" \
+    BuildFingerprint=OnePlus/CPH2691IN/OP5D3BL1:16/UKQ1.231108.001/V.R4T3.202512101801:user/release-keys \
+    DeviceName=OP5D3BL1 \
+    DeviceProduct=CPH2691 \
+    SystemDevice=OP5D3BL1 \
+    SystemName=CPH2691
